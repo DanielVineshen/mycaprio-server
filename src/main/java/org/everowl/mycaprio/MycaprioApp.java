e91725c7d4d6c28ec3f65c88743afc83908d893c
Daniel Vineshen <danielvineshen@gmail.com>
2025-01-23 16:06:38 +0800

    initial commit

---
+++ src/main/java/org/everowl/mycaprio/MycaprioApp.java
@@ -0,0 +1,11 @@
+package org.everowl.mycaprio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycaprioApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MycaprioApp.class, args);
+    }
+}
